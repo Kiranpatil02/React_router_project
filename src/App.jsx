8a9e3084c766fed8548b827d33b761731f40e276
Kiran <Kiranpatil73827@gmail.com>
2024-06-22 23:55:28 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=''>
+        hey there 
+      </div>
+    </>
+  )
+}
+
+export default App
